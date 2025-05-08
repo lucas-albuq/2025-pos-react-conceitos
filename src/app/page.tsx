2740c331926060c14e2d5285c7770e10e4e79e94
aluno <you@example.com>
2025-05-08 10:10:30 -0300

    P2 - Criar e aninhar componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,38 @@
 "use client";
 
+import React from "react";
+
 const Titulo = () => <h1>React - Conceitos básicos</h1>;
 
-const SubTitulo = () => (<h2>Lista de tarefas</h2>);
+const SubTitulo = () => <h2>Lista de tarefas</h2>;
 
-const Cabecalho = () => (
-  <>
-    <Titulo />
-    <SubTitulo />
-  </>
-)
+function Cabecalho() { 
+  return(
+    <>
+      <Titulo />
+      <SubTitulo />
+    </>
+  );
+}
+
+class Tarefa extends React.Component {
+    render(): React.ReactNode {
+        return (
+          <div>
+              <h3>Codar e codar durante 2025</h3>
+              <p>Pendente</p>
+          </div>
+        );
+    }
+}
 
 const Home = () => {
-  return <Cabecalho />;
+  return (
+    <>
+      <Cabecalho />
+      <Tarefa />
+    </>
+  );
 }
 
 export default Home;
