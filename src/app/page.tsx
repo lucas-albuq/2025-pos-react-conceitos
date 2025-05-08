fbbbd460c0fa40bd14b6b9e08a56f8d19c28b369
aluno <you@example.com>
2025-05-08 10:15:40 -0300

    P3 - Adicionando Estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,25 @@
 
 import React from "react";
 
-const Titulo = () => <h1>React - Conceitos básicos</h1>;
+const Titulo = () => <h1 className="text-2x1 font-bold mb-1">React - Conceitos básicos</h1>;
 
-const SubTitulo = () => <h2>Lista de tarefas</h2>;
+const SubTitulo = () => <h2 className="text-4x1 font-bold mb-6">Lista de tarefas</h2>;
 
 function Cabecalho() { 
   return(
-    <>
+    <div className="text-center">
       <Titulo />
       <SubTitulo />
-    </>
+    </div>
   );
 }
 
 class Tarefa extends React.Component {
     render(): React.ReactNode {
         return (
-          <div>
-              <h3>Codar e codar durante 2025</h3>
-              <p>Pendente</p>
+          <div className="p-3 mb-3 rounded-lg shadow-mb bg-gray-400">
+              <h3 className="text-x1 font-bold">Codar e codar durante 2025</h3>
+              <p className="text-sm">Pendente</p>
           </div>
         );
     }
@@ -28,10 +28,10 @@ class Tarefa extends React.Component {
 
 const Home = () => {
   return (
-    <>
+    <div className="container mx-auto p-4">
       <Cabecalho />
       <Tarefa />
-    </>
+    </div>
   );
 }
 
