8ffae58958912b1de661f5e4d0ca78d4b33e4a4c
Viviane <vivianethedev@gmail.com>
2025-05-12 11:27:14 -0300

    P6 - Respondendo a Eventos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,48 +2,60 @@
 
 import React from "react";
 
-const Titulo = () => <h1 className="text-2x1 font-bold mb-1">React - Conceitos básicos</h1>;
+const Titulo = () => (
+	<h1 className="text-2xl font-bold mb-1">React - Conceitos básicos</h1>
+);
 
-const SubTitulo = () => <h2 className="text-4x1 font-bold mb-6">Lista de tarefas</h2>;
+const SubTitulo = () => (
+	<h2 className="text-4xl font-bold mb-6">Lista de tarefas</h2>
+);
 
-function Cabecalho() { 
-  return(
-    <div className="text-center">
-      <Titulo />
-      <SubTitulo />
-    </div>
-  );
+function Cabecalho() {
+	return (
+		<div className="text-center">
+			<Titulo />
+			<SubTitulo />
+		</div>
+	);
 }
 
 interface TarefaProps {
-  titulo: string;
-    concluido?: boolean;
+	titulo: string;
+	concluido?: boolean;
 }
 
-class Tarefa extends React.Component<TarefaProps> {
-    render(): React.ReactNode {
-        return (
-          <div className="p-3 mb-3 rounded-lg shadow-mb bg-gray-400">
-              <h3 className="text-x1 font-bold">{this.props.titulo}</h3>
-              <p className="text-sm">{this.props.concluido ? "Concluída" : "Pendente"}</p>
-          </div>
-        );
-    }
-}
+const Tarefa: React.FC<TarefaProps> = ({ titulo, concluido }) => {
+	const classe = `p-3 mb-3 rounded-lg shadow-md hover:cursor-pointer hover:border ${
+		concluido
+			? "bg-gray-800 hover:border-gray-800"
+			: "bg-gray-400 hover:border-gray-400"
+	}`;
+
+	return (
+		<div
+			className={classe}
+			onClick={() => console.log(`A tarefa '${titulo}' foi clicada!`)}
+		>
+			<h3 className="text-xl font-bold">{titulo}</h3>
+			<p className="text-sm">{concluido ? "Concluída" : "Pendente"}</p>
+		</div>
+	);
+};
 
 const Home = () => {
-  const tarefas = [
-    { id: 1, title: "delectus aut autem", completed: false },
-    { id: 2, title: "quis ut nam facilis et officia qui", completed: true },
-    { id: 3, title: "fugiat veniam minus", completed: false },
-  ];
-  return (
-    <div className="container mx-auto p-4">
-      <Cabecalho />
-      <Tarefa titulo={tarefas[0].title} concluido={tarefas[0].completed} />
-      <Tarefa titulo={tarefas[1].title} concluido={tarefas[1].completed} />
-    </div>
-  );
-}
+	const tarefas = [
+		{ id: 1, title: "delectus aut autem", completed: false },
+		{ id: 2, title: "quis ut nam facilis et officia qui", completed: true },
+		{ id: 3, title: "fugiat veniam minus", completed: false },
+	];
+
+	return (
+		<div className="container mx-auto p-4">
+			<Cabecalho />
+			<Tarefa titulo={tarefas[0].title} concluido={tarefas[0].completed} />
+			<Tarefa titulo={tarefas[1].title} concluido={tarefas[1].completed} />
+		</div>
+	);
+};
 
 export default Home;
